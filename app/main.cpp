068565eb1cff4a31d7843c4e447225433be6a0fb
XzoRit <xzorit@hotmail.com>
2021-12-12 11:05:51 +0100

    show distribution of test support generators in app

--- app/main.cpp
+++ app/main.cpp
@@ -3,9 +3,17 @@
 
 #include <boost/program_options.hpp>
 
+#include <rapidcheck.h>
+
 #include <iostream>
 
+using ::rc::check;
 using ::xzr::lib::add;
+using ::xzr::lib::test::rand_day;
+using ::xzr::lib::test::rand_day_str;
+using ::xzr::lib::test::rand_month;
+using ::xzr::lib::test::rand_month_str;
+using ::xzr::lib::test::rand_year;
 using ::xzr::lib::test::rand_year_str;
 
 namespace po = boost::program_options;
@@ -38,9 +46,13 @@ int main(int ac, char* av[])
         std::cerr << "Exception of unknown type!\n";
     }
 
-    for (int i{}; i < 100; ++i)
-    {
-        std::cout << *rand_year_str;
-    }
+    check("rand_year", []() { RC_TAG(*rand_year); });
+    check("rand_month", []() { RC_TAG(*rand_month); });
+    check("rand_day", []() { RC_TAG(*rand_day); });
+
+    check("rand_year_str", []() { RC_TAG(*rand_year_str); });
+    check("rand_month_str", []() { RC_TAG(*rand_month_str); });
+    check("rand_day_str", []() { RC_TAG(*rand_day_str); });
+
     return add(0, 0);
 }
--- lib/lib/lib_test_support.cpp
+++ lib/lib/lib_test_support.cpp
@@ -17,7 +17,7 @@ namespace xzr::lib::test
 const Gen<int> rand_year{inRange(-9'999, 10'000).as("year")};
 const Gen<int> rand_month{inRange(1, 13).as("month")};
 const Gen<int> rand_day{inRange(1, 32).as("day")};
-const Gen<std::string> rand_year_str{container<std::string>(4, inRange<char>('0', 'A'))};
+const Gen<std::string> rand_year_str{container<std::string>(4, inRange<char>('0', '9' + 1))};
 const Gen<std::string> rand_month_str{map(rand_month, [](int a) { return std::to_string(a); }).as("month_str")};
 const Gen<std::string> rand_day_str{map(rand_day, [](int a) { return std::to_string(a); }).as("day_str")};
 }
